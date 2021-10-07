3d7aadd03844413339bb2854c21f4b573198cecb
Thomas Spaeth <thomas.spaeth01@gmail.com>
2021-10-06 23:36:38 -0400

    minor edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,21 +19,21 @@ function getResults(formData) {
 };
 
 function displaySearches(data) {
-    document.location.href = "search.html";
+    window.location.href = "/search.html";
     console.log(data.items);
     $("#questions").children("h3").text("Questions");
     console.log($("#questions").children("h3").text);
-    for (let index = 0; index < data.items.length; index++) {
-        /*var questionTitle = data.items[index].title;
+    /*for (let index = 0; index < data.items.length; index++) {
+        var questionTitle = data.items[index].title;
         var questionId = data.items[index].question_id;
         var question = $(`<div id="${questionId}">`)
         $("#question-list").text("Questions");
         $(question).text(questionTitle);
-        $("#question-list").append(question);*/
+        $("#question-list").append(question);
 
         //TODO: append titles to question-list section as buttons
         //TODO: create event listener for said buttons to display question details
-    };
+    };*/
 };
 
 $("#home_form").submit(function(event) {
